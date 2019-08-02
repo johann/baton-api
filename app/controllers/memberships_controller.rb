37e6444bc521405d007bf1ab1490c916f8bcdf3a
Johann <johannkerr@gmail.com>
2019-08-02 12:47:55 -0400

    add memberships

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,30 @@
 class MembershipsController < ApplicationController
+  before_action :set_membership, only: [:show, :delete]
+  def index
+    @memberships = Memberships.find_by(user_id: current_user.id)
+  end
+
+  def create
+    @membership = Membership.new(membership_params)
+    if @membership.save
+      render :show, status: :created, location: @membership
+    else
+      render json: @membership.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @membership.destroy
+  end
+
+  def set_membership
+    @membership = Memebrship.find(params[:id])
+  end
+
+  def membership_params
+    params.require(:membership).permit(:group_id, :user_id)
+  end
 end
