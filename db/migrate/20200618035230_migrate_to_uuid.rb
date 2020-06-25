251a13c25b63103f0cc3c36ea0e9b5ce9fcb9f61
Johann Kerr <johann@theskimm.com>
2020-06-24 21:22:37 -0400

    remove add primary key

--- db/migrate/20200618035230_migrate_to_uuid.rb
+++ db/migrate/20200618035230_migrate_to_uuid.rb
@@ -105,17 +105,6 @@ class MigrateToUuid < ActiveRecord::Migration[5.2]
     rename_column :memberships, :uuid, :id
     rename_column :attendances, :uuid, :id
 
-    execute "ALTER TABLE users ADD PRIMARY KEY (id);"
-    execute "ALTER TABLE groups ADD PRIMARY KEY (id);"
-    execute "ALTER TABLE activities ADD PRIMARY KEY (id);"
-    execute "ALTER TABLE memberships ADD PRIMARY KEY (id);"
-    execute "ALTER TABLE attendances ADD PRIMARY KEY (id);"
-
-    add_index :users, :created_at
-    add_index :groups, :created_at
-    add_index :activities, :created_at
-    add_index :attendances, :created_at
-    add_index :membership, :created_at
   end
 
   def down
