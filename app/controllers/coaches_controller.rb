fa96fedccbd8e59821c5df9f0c0e2af8f9c6ffdb
Johann Kerr <johannkerr@gmail.com>
2020-12-23 11:02:37 -0800

    add Create endpoint to show coaches that are not your coaches or you if you are a coach

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -2,7 +2,11 @@ class CoachesController < ApiController
   # before_action :set_group, only: [:show, :update]
 
   def index
-    @coaches = User.where(coach: true)
+    current_user_groups = current_user.groups
+    groups_without_current_user = Group.all.reject do |group|
+      current_user_groups.include?(group) || group.coach == current_user
+    end
+    @coaches = groups_without_current_user.map { |group| group.coach }
   end
 
   def show
