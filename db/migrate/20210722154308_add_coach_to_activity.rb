f50b488df3c828373e8137396b56c679c2a20d53
Johann Kerr <johannkerr@gmail.com>
2021-07-29 12:34:28 -0700

    update

--- db/migrate/20210722154308_add_coach_to_activity.rb
+++ db/migrate/20210722154308_add_coach_to_activity.rb
@@ -1,5 +1,5 @@
 class AddCoachToActivity < ActiveRecord::Migration[5.2]
   def change
-    add_reference :activities, :user_id, foreign_key: true, type: :uuid
+    add_reference :activities, :user, foreign_key: true, type: :uuid
   end
 end
