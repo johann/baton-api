41a3b9ccbe6b2f0e3056d9f238069a671c8b1a11
Johann Kerr <johannkerr@gmail.com>
2021-08-25 15:24:54 -0700

    check user id

--- app/controllers/coaches/activities_controller.rb
+++ app/controllers/coaches/activities_controller.rb
@@ -4,11 +4,11 @@ class Coaches::ActivitiesController < ApiController
       @user = User.find(params[:coach_id])
       head :not_found unless @user.is_coach?
       if params[:scope] == "past"
-        @activities = Activity.where(coach_id: @user.id, start_date: 12.months.ago..1.day.ago).order(:start_date).sort_by(&:start_date)
+        @activities = Activity.where(user_id: @user.id, start_date: 12.months.ago..1.day.ago).order(:start_date).sort_by(&:start_date)
       elsif params[:scope] == "future"
-        @activities = Activity.where(coach_id: @user.id, start_date: Date.today..12.months.from_now).order(:start_date)
+        @activities = Activity.where(user_id: @user.id, start_date: Date.today..12.months.from_now).order(:start_date)
       else
-        @activities = Activity.where(coach_id: @user.id)
+        @activities = Activity.where(user_id: @user.id)
       end
     rescue ActiveRecord::RecordNotFound
       head :not_found
