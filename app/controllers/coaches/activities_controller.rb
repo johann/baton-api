4f03b086eaf4568bd854e1549368e037effdd2b3
Johann Kerr <johannkerr@gmail.com>
2021-08-25 18:43:44 -0700

    remove dot

--- app/controllers/coaches/activities_controller.rb
+++ app/controllers/coaches/activities_controller.rb
@@ -6,7 +6,7 @@ class Coaches::ActivitiesController < ApiController
       if params[:scope] == "past"
         @activities = @user.coach_activities.where(start_date: 12.months.ago..1.day.ago).order(:start_date).sort_by(&:start_date)
       elsif params[:scope] == "future"
-        @activities = @user.coach_activities..where(start_date: Date.today..12.months.from_now).order(:start_date)
+        @activities = @user.coach_activities.where(start_date: Date.today..12.months.from_now).order(:start_date)
       else
         @activities = @user.coach_activities
       end
